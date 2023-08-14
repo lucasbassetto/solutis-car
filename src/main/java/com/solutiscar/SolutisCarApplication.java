18009ccb66ee41500889c1d0edd12e1417708d25
Lucas Bassetto <lucasfbassetto@gmail.com>
2023-08-14 20:02:32 -0300

    Project created

---
+++ src/main/java/com/solutiscar/SolutisCarApplication.java
@@ -0,0 +1,13 @@
+package com.solutiscar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutisCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutisCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solutiscar/SolutisCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutiscar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutisCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
