2a4b74e70a8c394b3854d38a6d66ab9e0791c7d1
Lucas Bassetto <lucasfbassetto@gmail.com>
2023-08-15 15:37:42 -0300

    PessoaService update

--- src/main/java/com/solutiscar/services/PessoaService.java
+++ src/main/java/com/solutiscar/services/PessoaService.java
@@ -1,6 +1,7 @@
 package com.solutiscar.services;
 
 
+import com.solutiscar.dto.PessoaDTO;
 import com.solutiscar.entities.Pessoa;
 import com.solutiscar.repositories.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,4 +11,26 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class PessoaService {
 
+    @Autowired
+    PessoaRepository pessoaRepository;
+
+    @Transactional
+    public PessoaDTO insert(PessoaDTO dto) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome(dto.getNome());
+        pessoa.setDate(dto.getDate());
+        pessoa.setCpf(dto.getCpf());
+
+        Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+
+        return convertEntityToDto(pessoaSalva);
+    }
+
+    private PessoaDTO convertEntityToDto(Pessoa pessoa) {
+        PessoaDTO dto = new PessoaDTO();
+        dto.setNome(pessoa.getNome());
+        dto.setCpf(pessoa.getNome());
+        dto.setDate(pessoa.getDate());
+        return dto;
+    }
 }
