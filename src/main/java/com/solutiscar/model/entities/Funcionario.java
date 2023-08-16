65ffeaeacd394754db003f3b58b974710a88da19
Lucas Bassetto <lucasfbassetto@gmail.com>
2023-08-16 08:36:21 -0300

    Adicionando anotações

--- src/main/java/com/solutiscar/model/entities/Funcionario.java
+++ src/main/java/com/solutiscar/model/entities/Funcionario.java
@@ -1,5 +1,6 @@
 package com.solutiscar.model.entities;
 
+import com.solutiscar.model.entities.enums.Sexo;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
@@ -9,13 +10,14 @@ import java.time.Instant;
 @Table(name = "tb_funcionario")
 public class Funcionario extends Pessoa {
 
+
     private String matricula;
 
     public Funcionario() {
     }
 
-    public Funcionario(Long id, String nome, Instant date, String cpf, String matricula) {
-        super(id, nome, date, cpf);
+    public Funcionario(Long id, String nome, Instant date, String cpf, Sexo sexo, String matricula) {
+        super(id, nome, date, cpf, sexo);
         this.matricula = matricula;
     }
 
--- src/main/java/com/solutiscar/model/entities/Motorista.java
+++ src/main/java/com/solutiscar/model/entities/Motorista.java
@@ -1,7 +1,10 @@
 package com.solutiscar.model.entities;
 
+import com.solutiscar.model.entities.enums.Sexo;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 import java.time.Instant;
 
@@ -9,13 +12,15 @@ import java.time.Instant;
 @Table(name = "tb_motorista")
 public class Motorista extends Pessoa {
 
+    @Size(min = 10, max = 10)
+    @Pattern(regexp = "[0-9]+")
     private String numeroCNH;
 
     public Motorista() {
     }
 
-    public Motorista(Long id, String nome, Instant date, String cpf, String numeroCNH) {
-        super(id, nome, date, cpf);
+    public Motorista(Long id, String nome, Instant date, String cpf, Sexo sexo, String numeroCNH) {
+        super(id, nome, date, cpf, sexo);
         this.numeroCNH = numeroCNH;
     }
 
@@ -26,6 +31,4 @@ public class Motorista extends Pessoa {
     public void setNumeroCNH(String numeroCNH) {
         this.numeroCNH = numeroCNH;
     }
-
-
 }
